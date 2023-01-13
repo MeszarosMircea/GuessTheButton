0472a29075b6b2923a84b716850103d0eb013b60
Meszaros Mircea Mihai <121308577+MeszarosMircea@users.noreply.github.com>
2023-01-13 21:31:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 var winningButton;
 var numberButtons;
 
-//adds the given number of buttons and picks a random number between 1 and 1000;
+//Adds the given number of buttons and picks a random number between 1 and 1000;
 function incrementButtons() {
   numberButtons = parseInt(document.getElementById("numberOfButtons").value);
   if (numberButtons <= 0 || numberButtons > 1000) {
-    document.getElementById("status").style.color = "red";
+    document.getElementById("status").style.color = "black";
     document.getElementById("status").innerHTML = "Status: Please restart and write a number between 1 and 1000!";
   } else if (numberButtons == 1) {
-    document.getElementById("status").style.color = "red";
+    document.getElementById("status").style.color = "black";
     document.getElementById("status").innerHTML = "Please, increase the number! &#9757;";
   } else {
     winningButton = Math.floor(Math.random() * numberButtons);
     for (let i = 1; i <= numberButtons; ++i) {
-      $("#buttons").append(`<button type="button" id=" + i + " class="btn btn-primary btn-lg" onclick = "return guessButton(` + i + `)">
-      Button: `+ i +`</button>`);
+      const button = document.createElement("button");
+      button.type = "button";
+      button.className = "btn btn-primary btn-lg";
+      button.textContent = `Button: ${i}`;
+      button.addEventListener("click", () => guessButton(i));
+      document.getElementById("buttons").appendChild(button);
     }
-    return false;
   }
 }
 
-//verifies if the button is winner or not
+//Verifies if the button is winner or not
 function guessButton(id) {
   if (id == winningButton) {
     document.getElementById("status").style.color = "white";
@@ -31,7 +34,7 @@ function guessButton(id) {
   }
 }
 
-//restarts the whole game
+//Restarts the whole game
 function restartGame() {
   location.reload();
 }
